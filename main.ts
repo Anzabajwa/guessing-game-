de8bd311761002a491c5d6b462e586aba1eb0eaf
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-05-21 13:34:43 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) computer will generate a random number 
+
+// 2) user input form guessing number
+
+// 3) campare user input with omputer generate number and show  result
+
+const randomNumber = Math.floor(Math.random()* 10+1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type:"number",
+        message: "Please guess a number between 1-10:",
+    },
+
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+
+    console.log("Congtratulations! you guessed right number.");
+
+}else{
+    console.log("you guessed wrong number");
+}
